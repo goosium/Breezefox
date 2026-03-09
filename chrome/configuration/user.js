c74e9a9ccd4a7362691b603acedf1c47dd691318
goose <118398255+goosium@users.noreply.github.com>
2026-03-08 21:21:29 -0400

    #1 attempt to get the icons to work

---
+++ chrome/configuration/user.js
@@ -0,0 +1,8 @@
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Enable SVG context-propertes
+user_pref("svg.context-properties.content.enabled", true);
+
+// Enable KDE file picker
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
